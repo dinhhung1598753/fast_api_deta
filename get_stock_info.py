66f3f9eafcbec6ccd0b5b47bccfc804ab0df3b65
hung.hoang <hoangdinhhung159@gmail.com>
2021-11-08 11:34:11 +0700

    push code

--- get_stock_info.py
+++ get_stock_info.py
@@ -12,7 +12,7 @@ def get_info(name):
         x = json.loads(r.text)
         result = []
         for i in x:
-            result.append(f"Mã cổ phiếu:{i['sym']} \n Tổng khối lượng giao dịch: {i['lot']} \n Giá tham chiếu:{i['r']}\n Giá Trần:{i['c']}\n Giá sàn:{i['f']}\n Giá Cao Nhất:{i['highPrice']}\n Giá Thấp Nhất:{i['lowPrice']}\n Bên mua:\n Giá 1:{i['g1']}\n Giá 2:{i['g2']}\n Giá 3:{i['g3']}\n Bên bán:\n Giá 1:{i['g4']}\n Giá 2:{i['g5']}\n Giá 3:{i['g6']}\n")
+            result.append(f"Mã cổ phiếu: {i['sym']} \n Tổng khối lượng giao dịch: {i['lot']} \n Giá tham chiếu: {i['r']}\n Giá Trần: {i['c']}\n Giá sàn: {i['f']}\n Giá Cao Nhất: {i['highPrice']}\n Giá Thấp Nhất: {i['lowPrice']}\n Bên mua:\n Giá 1: {i['g1']}\n Giá 2: {i['g2']}\n Giá 3: {i['g3']}\n Bên bán:\n Giá 1: {i['g4']}\n Giá 2: {i['g5']}\n Giá 3: {i['g6']}\n")
         return result[0]
     except:
         return f"error name :{name}"
