4bfcd491ba08398e09b85b8c13ab14a801cd092e
hung.hoang <hoangdinhhung159@gmail.com>
2021-11-08 13:11:07 +0700

    push code

--- get_stock_info.py
+++ get_stock_info.py
@@ -25,27 +25,55 @@ async def check_limit_price(price:str, name:str, chat_id):
     url = f"https://bgapidatafeed.vps.com.vn/getliststockdata/{name}"
     # for i in name:
     #     url.join(i+",")
+    i=0
     while True:
         try:
+            i+=1
             r = requests.get(url)
             x = json.loads(r.text)
-            if price == x[0]['lastPrice']:
-                result = []
-                for i in x:
-                    result.append(f"Mã cổ phiếu: {i['sym']} \n Tổng khối lượng giao dịch: {i['lot']} \n Giá hiện tại: {i['lastPrice']}\n Giá tham chiếu: {i['r']}\n Giá Trần: {i['c']}\n Giá sàn: {i['f']}\n Giá Cao Nhất: {i['highPrice']}\n Giá Thấp Nhất: {i['lowPrice']}\n Bên mua:\n Giá 1: {i['g1']}\n Giá 2: {i['g2']}\n Giá 3: {i['g3']}\n Bên bán:\n Giá 1: {i['g4']}\n Giá 2: {i['g5']}\n Giá 3: {i['g6']}\n")
-                
-                mes_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-                
-                payload = json.dumps({
-                "chat_id": chat_id,
-                "text": result[0]
-                })
-                # print(payload)
-                headers = {
-                'Content-Type': 'application/json'
-                }
+            last_price = float(x[0]['lastPrice'])
+            now_price = 0.0
+            if i == 1:
+                now_price = last_price
+            if now_price < float(price):
+                if float(price) <= last_price:
+                    result = []
+                    for i in x:
+                        result.append(f"Mã cổ phiếu: {i['sym']} \n Tổng khối lượng giao dịch: {i['lot']} \n Giá hiện tại: {i['lastPrice']}\n Giá tham chiếu: {i['r']}\n Giá Trần: {i['c']}\n Giá sàn: {i['f']}\n Giá Cao Nhất: {i['highPrice']}\n Giá Thấp Nhất: {i['lowPrice']}\n Bên mua:\n Giá 1: {i['g1']}\n Giá 2: {i['g2']}\n Giá 3: {i['g3']}\n Bên bán:\n Giá 1: {i['g4']}\n Giá 2: {i['g5']}\n Giá 3: {i['g6']}\n")
+                    
+                    mes_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+                    
+                    payload = json.dumps({
+                    "chat_id": chat_id,
+                    "text": result[0]
+                    })
+                    # print(payload)
+                    headers = {
+                    'Content-Type': 'application/json'
+                    }
 
-                response = requests.request("POST", mes_url, headers=headers, data=payload)
+                    response = requests.request("POST", mes_url, headers=headers, data=payload)
+                    break
+            else:
+                if float(price) >= last_price:
+                    result = []
+                    for i in x:
+                        result.append(f"Mã cổ phiếu: {i['sym']} \n Tổng khối lượng giao dịch: {i['lot']} \n Giá hiện tại: {i['lastPrice']}\n Giá tham chiếu: {i['r']}\n Giá Trần: {i['c']}\n Giá sàn: {i['f']}\n Giá Cao Nhất: {i['highPrice']}\n Giá Thấp Nhất: {i['lowPrice']}\n Bên mua:\n Giá 1: {i['g1']}\n Giá 2: {i['g2']}\n Giá 3: {i['g3']}\n Bên bán:\n Giá 1: {i['g4']}\n Giá 2: {i['g5']}\n Giá 3: {i['g6']}\n")
+                    
+                    mes_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+                    
+                    payload = json.dumps({
+                    "chat_id": chat_id,
+                    "text": result[0]
+                    })
+                    # print(payload)
+                    headers = {
+                    'Content-Type': 'application/json'
+                    }
+
+                    response = requests.request("POST", mes_url, headers=headers, data=payload)
+                    break
+            
             await asyncio.sleep(10)
             
         except:
