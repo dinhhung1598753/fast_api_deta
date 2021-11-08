ae6381d864998bdff42d54569a614a66348abbfa
hung.hoang <hoangdinhhung159@gmail.com>
2021-11-08 10:39:57 +0700

    push code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request
 from fastapi.responses import HTMLResponse
+import requests
 import time
 from get_stock_info import get_info
 import asyncio
@@ -73,3 +74,26 @@ async def send_m(websocket: WebSocket, stock_name: str):
         await websocket.send_text(f"{stock_name} --{i}")
         await asyncio.sleep(10)
 
+TOKEN = '2120867713:AAF7y9-CqPx0-ZI6MVSARkIv342N0TULTSA'  # Telegram Bot API Key
+
+def send_mess(chat_id,mess:str):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    
+    payload = json.dumps({
+    "chat_id": chat_id,
+    "text": mess
+    })
+    print(payload)
+    headers = {
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+
+@app.post("/webhook")
+async def recWebHook(req: Request):
+    body = await req.json()
+    id = body['message']['chat']['id']
+    sender_text = body['message']['text']
+    send_mess(id,mess="mèo meo meo mèo meo")
